05a5bc2243b9c64df248f24b31a7833446c930fe
Rene Silva <rene.sil@gmail.com>
2021-11-12 23:07:04 -0400

    ejercicio 1 vue

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,60 @@
+const arreglarHora = (valor) => {
+  if (valor < 10) {
+    return '0' + valor;
+  } else {
+    return valor;
+  }
+};
+
 // Vue Ejercicio Horas
 const EjHoras = {
   data() {
-    return {};
+    return {
+      sumH: 2,
+      sumM: 30,
+      sumS: 30,
+      horas: 0,
+      minutos: 0,
+      segundos: 0,
+      mensaje: '',
+    };
   },
-  methods: {},
+  computed: {
+    resultado() {
+      let cS = 0;
+      let cM = 0;
+
+      let finalS = this.sumS + this.segundos;
+      if (finalS >= 60) {
+        finalS = finalS - 60;
+        cS = 1;
+      }
+
+      let finalM = this.sumM + this.minutos + cS;
+      if (finalM >= 60) {
+        finalM = finalM - 60;
+        cM = 1;
+      }
+
+      let finalH = this.sumH + this.horas + cM;
+      if (finalH >= 12) {
+        finalH = finalH - 12;
+      }
+
+      return [finalH, finalM, finalS];
+    },
+  },
+  methods: {
+    calcular() {
+      this.mensaje =
+        'La hora calculada es: ' +
+        arreglarHora(this.resultado[0]) +
+        ':' +
+        arreglarHora(this.resultado[1]) +
+        ':' +
+        arreglarHora(this.resultado[2]);
+    },
+  },
 };
 Vue.createApp(EjHoras).mount('#ejercicio-horas');
 
