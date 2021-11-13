75e38bfd3248da31bf1ca77914f0f4131c8a1b9e
Rene Silva <rene.sil@gmail.com>
2021-11-12 22:43:04 -0400

    base para vue

---
+++ src/assets/js/main.js
@@ -0,0 +1,26 @@
+// Vue Ejercicio Horas
+const EjHoras = {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+Vue.createApp(EjHoras).mount('#ejercicio-horas');
+
+// Vue Ejercicio Lista de Compras
+const EjListaCompras = {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+Vue.createApp(EjListaCompras).mount('#ejercicio-lista-compras');
+
+// Vue Ejercicio Calculadora
+const EjCalculadora = {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+Vue.createApp(EjCalculadora).mount('#ejercicio-calculadora');
