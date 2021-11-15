b35d5e8c15bae5bdc24557065490ac9ace82d101
Eressea RS <rsilva@eresseasolutions.com>
2021-11-15 15:56:09 -0400

    Adición de archivos CSS

--- gulpfile.js
+++ gulpfile.js
@@ -12,56 +12,75 @@ const sass = require('gulp-sass')(require('sass'));
 // watch files
 const htmlWatchFiles = './src/*.html';
 const javascriptWatchFiles = './src/assets/js/*.js';
-const vendorJavascriptFiles = './vendor/js/*.js';
-const mapsVendorJavascriptFiles = './vendor/js/*.js.map';
 const sourceFileSass = './src/assets/scss/stylesheet.scss';
-const cssStyleWatchFiles = './src/assets/scss/**/*.scss';
+const sassStyleWatchFiles = './src/assets/scss/**/*.scss';
+const vendorMapsJavascriptFiles = './vendor/js/*.js.map';
+const vendorJavascriptFiles = './vendor/js/*.js';
+const vendorCssFiles = './vendor/css/**/*.css';
 const vendorFontsFiles = './vendor/fonts/**/*';
 
-// CSS
-const css = function() {
-  return gulp.src(sourceFileSass)
-    .pipe(changed(cssStyleWatchFiles))
+// SASS
+const sassFiles = function () {
+  return gulp
+    .src(sourceFileSass)
+    .pipe(changed(sassStyleWatchFiles))
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./src/static/css/'))
     .pipe(browserSync.stream());
 };
 
+// Vendor CSS
+const vendorCss = function () {
+  return gulp
+    .src(vendorCssFiles)
+    .pipe(changed(vendorCssFiles))
+    .pipe(concat('vendors.css'))
+    .pipe(
+      rename({
+        extname: '.css',
+      }),
+    )
+    .pipe(gulp.dest('./src/static/css/'))
+    .pipe(browserSync.stream());
+};
+
 // Vendor JS
-const vendorJS = function() {
-  return gulp.src(vendorJavascriptFiles)
+const vendorJS = function () {
+  return gulp
+    .src(vendorJavascriptFiles)
     .pipe(changed(vendorJavascriptFiles))
     .pipe(gulp.dest('./src/static/js/'))
     .pipe(browserSync.stream());
 };
 
-// Maps Vendor JS
-const mapsVendorJS = function() {
-  return gulp.src(mapsVendorJavascriptFiles)
-    .pipe(changed(mapsVendorJavascriptFiles))
+// Vendor Maps JS
+const vendorMapsJS = function () {
+  return gulp
+    .src(vendorMapsJavascriptFiles)
+    .pipe(changed(vendorMapsJavascriptFiles))
     .pipe(gulp.dest('./src/static/js/'))
     .pipe(browserSync.stream());
 };
 
 // Vendor Fonts
-const vendorFonts = function() {
-  return gulp.src(vendorFontsFiles)
+const vendorFonts = function () {
+  return gulp
+    .src(vendorFontsFiles)
     .pipe(changed(vendorFontsFiles))
     .pipe(gulp.dest('./src/static/fonts/'))
     .pipe(browserSync.stream());
 };
 
 // watch gulp
-const watchGulp = function() {
+const watchGulp = function () {
   gulp.watch(htmlWatchFiles).on('change', browserSync.reload);
-  gulp.watch(cssStyleWatchFiles, css);
+  gulp.watch(sassStyleWatchFiles, sassFiles);
   gulp.watch(javascriptWatchFiles).on('change', browserSync.reload);
 };
 exports.watchGulp = watchGulp;
 
-
 // Subir al servidor
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
       baseDir: './src/',
@@ -69,5 +88,16 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('build',  gulp.parallel(css, vendorJS, mapsVendorJS, vendorFonts));
-gulp.task('default', gulp.parallel(css, vendorJS, mapsVendorJS, vendorFonts, 'browser-sync', watchGulp));
+gulp.task('build', gulp.parallel(sassFiles, vendorCss, vendorJS, vendorMapsJS, vendorFonts));
+gulp.task(
+  'default',
+  gulp.parallel(
+    sassFiles,
+    vendorCss,
+    vendorJS,
+    vendorMapsJS,
+    vendorFonts,
+    'browser-sync',
+    watchGulp,
+  ),
+);
