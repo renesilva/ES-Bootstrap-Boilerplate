d45c391d3cfac97808b27c997a52cab334fa43e5
Eressea RS <rsilva@eresseasolutions.com>
2021-11-20 10:25:33 -0400

    Ejercicios base

---
+++ src/assets/js/ej4.js
@@ -0,0 +1,12 @@
+const AppComponent = {
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+};
+const app = Vue.createApp(AppComponent);
+const vm = app.mount('#app');
+
+// También podemos crearlo de esta manera Vue.createApp(AppComponent).mount('#app');
