3dd28d7ece44a8a3b6e6ffa0a17a007ca16747af
Eressea RS <rsilva@eresseasolutions.com>
2021-11-20 10:33:06 -0400

    Ejercicio 1

--- src/assets/js/ej1.js
+++ src/assets/js/ej1.js
@@ -1,9 +1,58 @@
 const AppComponent = {
   data() {
-    return {};
+    return {
+      sumH: 2,
+      sumM: 30,
+      sumS: 30,
+      horas: 0,
+      minutos: 0,
+      segundos: 0,
+      mensaje: '',
+    };
   },
-  methods: {},
-  computed: {},
+  methods: {
+    calcular() {
+      this.mensaje =
+        'La hora calculada es: ' +
+        this.arreglarHora(this.resultado[0]) +
+        ':' +
+        this.arreglarHora(this.resultado[1]) +
+        ':' +
+        this.arreglarHora(this.resultado[2]);
+    },
+    arreglarHora(valor) {
+      if (valor < 10) {
+        return '0' + valor;
+      } else {
+        return valor;
+      }
+    },
+  },
+  computed: {
+    resultado() {
+      let cS = 0;
+      let cM = 0;
+
+      let finalS = this.sumS + this.segundos;
+      if (finalS >= 60) {
+        finalS = finalS - 60;
+        cS = 1;
+      }
+
+      let finalM = this.sumM + this.minutos + cS;
+      if (finalM >= 60) {
+        finalM = finalM - 60;
+        cM = 1;
+      }
+
+      let finalH = this.sumH + this.horas + cM;
+      if (finalH >= 12) {
+        finalH = finalH - 12;
+      }
+
+      return [finalH, finalM, finalS];
+    },
+  },
   watch: {},
 };
 const app = Vue.createApp(AppComponent);
