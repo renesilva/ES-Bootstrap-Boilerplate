32a2315a4faa10c7d0c6f422d35b5b8626fc6625
Eressea RS <rsilva@eresseasolutions.com>
2021-11-20 12:51:01 -0400

    Ejercicio 2

--- src/assets/js/ej2.js
+++ src/assets/js/ej2.js
@@ -1,10 +1,37 @@
 const AppComponent = {
   data() {
-    return {};
+    return {
+      carousel: null,
+      slides: [
+        {
+          image: 'assets/images/code-1.jpg',
+          title: 'Primero!',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        },
+        {
+          image: 'assets/images/code-2.jpg',
+          title: 'Segundo!',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        },
+        {
+          image: 'assets/images/code-3.jpg',
+          title: 'Tercero!',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        },
+      ],
+    };
   },
   methods: {},
   computed: {},
   watch: {},
+  mounted() {
+    const carouselElement = document.querySelector('#carouselEj2');
+    this.carousel = bootstrap.Carousel.getOrCreateInstance(carouselElement, {
+      interval: 1000,
+      ride: 'carousel',
+    });
+    this.carousel.cycle();
+  },
 };
 const app = Vue.createApp(AppComponent);
 const vm = app.mount('#app');
