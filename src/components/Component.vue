73df677badf535e262caeee49368a918b5b4ee8f
Eressea RS <rsilva@eresseasolutions.com>
2021-11-23 08:21:46 -0400

    Archivo Component.vue de ejemplo

---
+++ src/components/Component.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  mounted() {},
+  // Se pueden utilizar estos hooks para el ciclo de vida
+  // beforeCreate, created, beforeMount, mounted, beforeUpdate, updated
+  // activated, deactivated, beforeUnmount, unmounted
+};
+</script>
+
+<style scoped>
+
+</style>
